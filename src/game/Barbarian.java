940c990a7b9ba66549196ff35907a8061dcc66a4
cortazar <rebeca.cortazar@deusto.es>
2015-03-20 23:44:37 +0100

    remove points

--- src/game/Barbarian.java
+++ src/game/Barbarian.java
@@ -9,6 +9,6 @@ public class Barbarian extends Character {
 	@Override
 	public void display() {
 	// TODO Auto-generated method stub
-		System.out.println("You are a strong, hulky barbarian .........");
+		System.out.println("You are a strong, hulky barbarian ..");
 	}
 }
