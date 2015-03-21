1d252f14825f7b341f2350ba43f8bce738862a16
cortazar <rebeca.cortazar@deusto.es>
2015-03-21 15:02:12 +0100

    ca

--- src/game/Barbarian.java
+++ src/game/Barbarian.java
@@ -11,5 +11,7 @@ public class Barbarian extends Character {
 	// TODO Auto-generated method stub
 
 		System.out.println("You are a strong, hulky barbarian.");
-		String s = "user a añade una linea diferente"		int i= 1;	}
+		String s = "user a añade una linea diferente";
+
+	}
 }
