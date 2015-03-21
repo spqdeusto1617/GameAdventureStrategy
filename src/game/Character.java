07fc6593b2f247122e11c7e377b9d94d70c04fd7
cortazar <rebeca.cortazar@deusto.es>
2015-03-21 16:49:49 +0100

    original state for Character.java

--- src/game/Character.java
+++ src/game/Character.java
@@ -12,7 +12,7 @@ public abstract class Character {
 		}
 		public void setWeapon(WeaponBehaviour w){
 			System.out.println("Let´s change our weapons ...");
-System.out.println("Cambios en A");System.out.println("Cambios en B");			weapon = w;
+		weapon = w;
 		}
 		public void fight(){ 
 	        weapon.useWeapon();
