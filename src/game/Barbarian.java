6e54a5613d787fbf5decc5bebc41e4c162f81f58
cortazar <rebeca.cortazar@deusto.es>
2015-03-21 14:41:12 +0100

    user b linea sin conflicto aparente

--- src/game/Barbarian.java
+++ src/game/Barbarian.java
@@ -11,6 +11,6 @@ public class Barbarian extends Character {
 	// TODO Auto-generated method stub
 
 		System.out.println("You are a strong, hulky barbarian.");
-		System.out.println("User B añade una linea sin conflicto.");
+		System.out.println("User B añade una linea sin conflicto aparente.");
 	}
 }
