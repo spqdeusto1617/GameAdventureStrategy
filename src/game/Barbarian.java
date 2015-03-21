f7db6f122bce6dd0febc83ba761ccc3c49d93578
cortazar <rebeca.cortazar@deusto.es>
2015-03-21 14:28:49 +0100

    User a borra linea y puntos. vuelta al inicio

--- src/game/Barbarian.java
+++ src/game/Barbarian.java
@@ -10,8 +10,6 @@ public class Barbarian extends Character {
 	public void display() {
 	// TODO Auto-generated method stub
 
-		System.out.println("You are a strong, hulky barbarian. Extra");
-
-		System.out.println("User a adds one line ");
+		System.out.println("You are a strong, hulky barbarian.");
 	}
 }
