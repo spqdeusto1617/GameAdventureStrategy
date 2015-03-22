7754ab3df346a6e748b7cba70179e0342f23e19a
cortazar <rebeca.cortazar@deusto.es>
2015-03-22 20:40:24 +0100

    añadida linea sysos 1 en Character.java

--- src/game/Character.java
+++ src/game/Character.java
@@ -12,6 +12,7 @@ public abstract class Character {
 		}
 		public void setWeapon(WeaponBehaviour w){
 			System.out.println("Let´s change our weapons ...");
+						System.out.println("LCambios en Character");
 		weapon = w;
 		}
 		public void fight(){ 
