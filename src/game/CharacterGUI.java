1fc205040397b3f0cda1628d1fdee3c68abf288f
cortazar <rebeca.cortazar@deusto.es>
2017-03-26 18:41:24 +0200

    Task done #RCS17-11 Done

--- src/game/CharacterGUI.java
+++ src/game/CharacterGUI.java
@@ -4,6 +4,6 @@ public class CharacterGUI {
 	
 	// Test 1, let's simulate that I have been working on this class for 1 hour and I wish to log the time
 
-//Let´s simulate that the task is over and that we can change its state to Fixed
+//Test 2 - Let´s simulate that the task is over and that we can change its state to Done
 		
 }
