31147a0a2bab2444b595c620dffa7887f46b4e46
cortazar <rebeca.cortazar@deusto.es>
2017-03-26 18:26:38 +0200

    Adding working time to #RCS17-11 add work 1h Development

--- src/game/CharacterGUI.java
+++ src/game/CharacterGUI.java
@@ -1,6 +1,8 @@
 package game;
 
 public class CharacterGUI {
+	
+	// Test 1, let's simulate that I have been working on this class for 1 hour and I wish to log the time
 
 //Let´s simulate that the task is over and that we can change its state to Fixed
 		
