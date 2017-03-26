aa01dd4adc5f95ccb3d1d36a0cc16663d7389295
cortazar <rebeca.cortazar@deusto.es>
2017-03-26 19:09:49 +0200

    Getting Barbarian.java back. Revert to parent

---
+++ src/game/Barbarian.java
@@ -0,0 +1,17 @@
+package game;
+
+public class Barbarian extends Character {
+
+	public Barbarian(WeaponBehaviour w) {
+		super(w);
+	}
+	
+	@Override
+	public void display() {
+	// TODO Auto-generated method stub
+
+		System.out.println("You are a strong, hulky barbarian.");
+		System.out.println("Some changes in master branch.");
+		
+	}
+}
